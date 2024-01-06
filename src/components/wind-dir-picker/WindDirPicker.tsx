7d526ee8fd653165adc10c013ffd975571a26c9b
o-murphy <thehelixpg@gmail.com>
2024-01-06 05:14:39 +0200

    circular picker replaced

--- src/components/wind-dir-picker/WindDirPicker.tsx
+++ src/components/wind-dir-picker/WindDirPicker.tsx
@@ -2,6 +2,7 @@ import {Text, useTheme} from "react-native-paper";
 import React from "react";
 import {View} from "react-native";
 import CircularSlider from "../circular-slider";
+import CircleSlider from "../circular-slider/CircleSlider";
 
 
 export default function WindDirection({curValue, onChange}) {
@@ -18,37 +19,55 @@ export default function WindDirection({curValue, onChange}) {
             marginTop: -110,
             fontWeight: "bold"
         },
-        slider: {
-        },
+        slider: {},
     }
 
     return (
 
         <View style={styles.container}>
-                <CircularSlider
-                    // style={styles.slider}
+            {/*<CircularSlider*/}
+            {/*    // style={styles.slider}*/}
 
-                    size={200}
-                    trackWidth={15}
-                    minValue={0}
-                    maxValue={12}
-                    startAngle={0}
-                    endAngle={360}
-                    angleType={{
-                        direction: "cw",
-                        axis: "+y"
-                    }}
-                    handle1={{
-                        value: curValue,
-                        onChange: onChange
-                    }}
-                    coerceToInt={true}
-                    // arcColor={theme.colors.onSecondaryContainer}
-                    arcColor={theme.colors.secondaryContainer}
-                    arcBackgroundColor={theme.colors.secondaryContainer}
-                />
+            {/*    size={200}*/}
+            {/*    trackWidth={15}*/}
+            {/*    minValue={0}*/}
+            {/*    maxValue={12}*/}
+            {/*    startAngle={0}*/}
+            {/*    endAngle={360}*/}
+            {/*    angleType={{*/}
+            {/*        direction: "cw",*/}
+            {/*        axis: "+y"*/}
+            {/*    }}*/}
+            {/*    handle1={{*/}
+            {/*        value: curValue,*/}
+            {/*        onChange: onChange*/}
+            {/*    }}*/}
+            {/*    coerceToInt={true}*/}
+            {/*    // arcColor={theme.colors.onSecondaryContainer}*/}
+            {/*    arcColor={theme.colors.secondaryContainer}*/}
+            {/*    arcBackgroundColor={theme.colors.secondaryContainer}*/}
+            {/*/>*/}
+
+
+            <CircleSlider value={curValue}
+                          dialRadius={100}
+                          btnRadius={18}
+                          min={0}
+                          max={359}
+                          dialWidth={15}
+                          meterColor={theme.colors.secondaryContainer}
+                          textColor={theme.colors.onSecondaryContainer}
+                //           fillColor={theme.colors.secondaryContainer}
+                //           btnColor={theme.colors.secondaryContainer}
+                          strokeColor={theme.colors.secondaryContainer}
+                          strokeWidth={20}
+                          textSize={0}
+                          onValueChange={value => onChange(Math.round(value / 30))}
+                          style={[styles.slider, {padding: 5}]}/>
+
 
             <Text variant={"titleMedium"} style={styles.text}>{`${curValue * 30}° (${curValue}h)`}</Text>
+
 
         </View>
     )
