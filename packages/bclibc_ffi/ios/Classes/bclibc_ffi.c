fe03d7c5eb3d327ed2f2c7593351f2ab3ddbacc7
o-murphy <thehelixpg@gmail.com>
2026-07-01 09:31:40 +0300

    removed: bclibc_ffi local package

--- packages/bclibc_ffi/ios/Classes/bclibc_ffi.c
+++
@@ -1,3 +0,0 @@
-// Relative import to be able to reuse the C sources.
-// See the comment in ../bclibc_ffi.podspec for more information.
-#include "../../src/bclibc_ffi.c"
--- packages/bclibc_ffi/lib/unit.dart
+++
@@ -1,1 +0,0 @@
-export 'package:bclibc_ffi/src/unit.dart';
